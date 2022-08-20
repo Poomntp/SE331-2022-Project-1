f44f143dbb1a126e22e32ffcb51fe6a6bb71a30e
NatchaPetchsikul <natcha_pet@cmu.ac.th>
2022-08-20 21:09:51 +0700

    home page

--- src/services/EventService.js
+++ src/services/EventService.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 const apiClient = axios.create({
-  baseURL: "https://my-json-server.typicode.com/little0011/SE331-2022-Project-1/peoples",
+  baseURL:
+    'https://my-json-server.typicode.com/little0011/SE331-2022-Project-1/peoples',
   withCredentials: false,
   headers: {
     Accept: 'application/json',
@@ -11,7 +12,7 @@ const apiClient = axios.create({
 
 export default {
   getEvents(perPage, page) {
-    return apiClient.get('/peoples?_limit=' + perPage + '&_page=' + page)
+    return apiClient.get('?_limit=' + perPage + '&_page=' + page)
   },
   //Added new call
   getEvent(id) {
