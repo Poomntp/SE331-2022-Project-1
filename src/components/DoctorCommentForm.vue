aa49dd920397188b3b9529db9b638bf7c67f679b
POOM <nitipoom_t@cmu.ac.th>
2022-08-23 05:37:38 +0700

    Decorate DocterCommentForm

--- src/components/DoctorCommentForm.vue
+++ src/components/DoctorCommentForm.vue
@@ -1,12 +1,21 @@
 <template>
   <form class="review-form" @submit.prevent="onSubmit">
-    <h3>Leave a review</h3>
-    <label for="name">Name:</label>
-    <input id="name" v-model="name" />
-    <label for="review">Comment:</label>
-    <textarea id="review" v-model="review"></textarea>
-
-    <input class="button" type="submit" value="Submit" />
+    <div class="title">
+      <h2>COMMENT</h2>
+    </div>
+    <div class="half">
+      <div class="item">
+        <label for="name">NAME</label>
+        <input id="name" v-model="name" />
+      </div>
+    </div>
+    <div class="full">
+      <label for="review">MESSAGE</label>
+      <textarea id="review" v-model="review"></textarea>
+    </div>
+    <div class="action">
+      <input type="submit" value="Submit" />
+    </div>
   </form>
 </template>
 
@@ -43,33 +52,110 @@ export default {
 </script>
 
 <style>
-.button {
-  font-size: 1.7rem;
-  padding: 10px 30px;
-  background: none;
-  border: white solid 2px;
-  color: white;
-  font-weight: 700;
-  letter-spacing: 2px;
-  position: relative;
-  overflow: hidden;
-  cursor: pointer;
-}
-.button::after {
-  content: '';
-  width: 0;
-  height: 100%;
-  position: absolute;
-  background: white;
-  left: 0;
-  top: 0;
-  z-index: -1;
-  transition: 0.4s;
-}
-.button:hover {
+h3 {
   color: black;
 }
-.button:hover::after {
+
+form {
+  background: rgba(27, 31, 34, 0.8);
+  width: 640px;
+  margin: 50px auto;
+  max-width: 97%;
+  border-radius: 4px;
+  padding: 55px 30px;
+  color: white;
+}
+
+form .title h2 {
+  letter-spacing: 6px;
+  border-bottom: 1px solid white;
+  display: inline-block;
+  padding-bottom: 8px;
+  margin-bottom: 32px;
+}
+
+form .half {
+  display: flex;
+  justify-content: space-between;
+}
+
+form .half .item {
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 24px;
+  width: 48%;
+}
+
+form .half .item input {
+  border-radius: 4px;
+  border: 1px solid white;
+  outline: 0;
+  padding: 16px;
   width: 100%;
+  height: 44px;
+  background: transparent;
+  font-size: 17px;
+}
+
+form .full {
+  margin-bottom: 24px;
+}
+
+form .full textarea {
+  background: transparent;
+  border-radius: 4px;
+  border: 1px solid white;
+  outline: 0;
+  padding: 12px 16px;
+  width: 100%;
+  height: 200px;
+  font-size: 17px;
+}
+
+form .action {
+  margin-bottom: 32px;
+}
+
+form .action input {
+  background: transparent;
+  border-radius: 4px;
+  border: 1px solid white;
+  cursor: pointer;
+  font-size: 13px;
+  font-weight: 600;
+  height: 44px;
+  letter-spacing: 3px;
+  outline: 0;
+  padding: 0 20px 0 22px;
+  margin-right: 10px;
+}
+
+form .action input[type='submit'] {
+  background: white;
+  color: black;
+}
+
+form .half .item input:focus,
+form .full textarea:focus,
+form .action input[type='reset']:hover,
+form .icons a:hover {
+  background: rgba(255, 255, 255, 0.075);
+}
+
+@media (max-width: 480px) {
+  form .half {
+    flex-direction: column;
+  }
+  form .half .item {
+    width: 100%;
+  }
+  form .action {
+    display: flex;
+    flex-direction: column;
+  }
+  form .action input {
+    margin-bottom: 10px;
+    width: 100%;
+  }
 }
 </style>
--- src/components/DoctorCommentView.vue
+++ src/components/DoctorCommentView.vue
@@ -1,5 +1,18 @@
 <template>
-  <div class="review-container">
+  <div class="dialogbox">
+    <div class="body">
+      <span class="tip tip-left"></span>
+      <div class="message">
+        <span
+          ><li v-for="(review, index) in reviews" :key="index">
+            {{ review.name }}
+            "{{ review.review }}"
+          </li></span
+        >
+      </div>
+    </div>
+  </div>
+  <!-- <div class="review-container">
     <h3>Comment:</h3>
     <ul>
       <li v-for="(review, index) in reviews" :key="index">
@@ -7,7 +20,7 @@
         "{{ review.review }}"
       </li>
     </ul>
-  </div>
+  </div> -->
 </template>
 
 <script>
@@ -15,3 +28,41 @@ export default {
   props: ['reviews']
 }
 </script>
+
+<style scoped>
+.tip {
+  width: 0px;
+  height: 0px;
+  position: absolute;
+  background: transparent;
+  border: 10px solid #ccc;
+}
+.tip-left {
+  top: 10px;
+  left: -25px;
+  border-top-color: transparent;
+  border-left-color: transparent;
+  border-bottom-color: transparent;
+}
+
+.body .message {
+  min-height: 30px;
+  border-radius: 3px;
+  font-family: Arial;
+  font-size: 14px;
+  line-height: 1.5;
+  color: #797979;
+}
+
+.dialogbox .body {
+  position: relative;
+  max-width: 300px;
+  height: auto;
+  margin: auto;
+  padding: 5px;
+  background-color: #dadada;
+  border-radius: 3px;
+  border: 5px solid #ccc;
+  margin-bottom: 10px;
+}
+</style>
