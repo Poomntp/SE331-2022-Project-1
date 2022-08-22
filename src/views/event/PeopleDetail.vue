c1eb26a96e6ea18f210f34fcf6137c1e702409b7
POOM <nitipoom_t@cmu.ac.th>
2022-08-23 05:38:19 +0700

    Edit PeopleDetail

--- src/views/event/PeopleDetail.vue
+++ src/views/event/PeopleDetail.vue
@@ -65,35 +65,6 @@ h4 {
   font-size: 40px;
   color: white;
 }
-button {
-  font-size: 1.7rem;
-  padding: 10px 30px;
-  background: none;
-  border: white solid 2px;
-  color: white;
-  font-weight: 700;
-  letter-spacing: 2px;
-  position: relative;
-  overflow: hidden;
-  cursor: pointer;
-}
-button::after {
-  content: '';
-  width: 0;
-  height: 100%;
-  position: absolute;
-  background: white;
-  left: 0;
-  top: 0;
-  z-index: -1;
-  transition: 0.4s;
-}
-button:hover {
-  color: black;
-}
-button:hover::after {
-  width: 100%;
-}
 h2 {
   color: black;
 }
